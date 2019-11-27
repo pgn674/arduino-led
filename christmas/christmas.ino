5983ef119f84da7c5b4935d0649c3e4ffe4c6c24
Paul Nickerson <pnickerson@cashstar.com>
2019-11-27 16:19:57 -0500

    Added some faster pulses.

--- christmas/christmas.ino
+++ christmas/christmas.ino
@@ -3,14 +3,18 @@
 #define NUM_LEDS 50
 #define OFF_TIME 1020
 
+// It makes sense for OFF_TIME to be a multiple of 255
+
 CRGB leds[NUM_LEDS];
 CRGBSet ledset(leds, NUM_LEDS);
 
 bool pulsing[NUM_LEDS];
 int color[NUM_LEDS];
 int goingDirection[NUM_LEDS];
-int pulseDelay[NUM_LEDS];
+int pulseLength[NUM_LEDS];
 int delayCounter[NUM_LEDS];
+int skipUp[NUM_LEDS];
+int ledToBe;
 
 void setup() {
   randomSeed(analogRead(0));
@@ -36,49 +40,60 @@ void loop() {
         pulsing[led_select] = true;
         color[led_select] = random(3);
         goingDirection[led_select] = 1;
-        pulseDelay[led_select] = random(3);
-        delayCounter[led_select] = pulseDelay[led_select];
+        pulseLength[led_select] = random(5);
+        if (pulseLength[led_select] >= 2) {
+          delayCounter[led_select] = pulseLength[led_select] - 2;
+          skipUp[led_select] = 1;
+        } else {
+          skipUp[led_select] = delayCounter[led_select] * -1 + 3;
+        }
       }
     } else {
-      if (delayCounter[led_select] != 0) {
+      if (pulseLength[led_select] >= 2 && delayCounter[led_select] != 0) {
         delayCounter[led_select] = delayCounter[led_select] - 1;
       } else {
         switch (color[led_select]) {
           case 0:  // Red
-            if (leds[led_select].red == 1 && goingDirection[led_select] == -1) {
-              leds[led_select].red = 0;
-              pulsing[led_select] = false;
-            } else if (leds[led_select].red == 255) {
+            if (leds[led_select].red == 255) {
               goingDirection[led_select] = -1;
-              leds[led_select].red = leds[led_select].red + goingDirection[led_select];
-            } else {
-              leds[led_select].red = leds[led_select].red + goingDirection[led_select];
             }
+            ledToBe = leds[led_select].red + goingDirection[led_select] * skipUp[led_select];
+            if (ledToBe > 255) {
+              ledToBe = 255;
+            } else if (ledToBe <= 0) {
+              ledToBe = 0;
+              pulsing[led_select] = false;
+            }
+            leds[led_select].red = ledToBe;
             break;
           case 1:  // Green
-            if (leds[led_select].green == 1 && goingDirection[led_select] == -1) {
-              leds[led_select].green = 0;
-              pulsing[led_select] = false;
-            } else if (leds[led_select].green == 255) {
+            if (leds[led_select].green == 255) {
               goingDirection[led_select] = -1;
-              leds[led_select].green = leds[led_select].green + goingDirection[led_select];
-            } else {
-              leds[led_select].green= leds[led_select].green + goingDirection[led_select];
             }
+            ledToBe = leds[led_select].green + goingDirection[led_select] * skipUp[led_select];
+            if (ledToBe > 255) {
+              ledToBe = 255;
+            } else if (ledToBe <= 0) {
+              ledToBe = 0;
+              pulsing[led_select] = false;
+            }
+            leds[led_select].green = ledToBe;
             break;
           case 2:  // White
-            if (leds[led_select].blue == 1 && goingDirection[led_select] == -1) {
-              leds[led_select] = CRGB(0, 0, 0);
-              pulsing[led_select] = false;
-            } else if (leds[led_select].blue == 255) {
+            if (leds[led_select].blue == 255) {
               goingDirection[led_select] = -1;
-              leds[led_select] = CRGB(leds[led_select].blue + goingDirection[led_select], leds[led_select].blue + goingDirection[led_select], leds[led_select].blue + goingDirection[led_select]);
-            } else {
-              leds[led_select] = CRGB(leds[led_select].blue + goingDirection[led_select], leds[led_select].blue + goingDirection[led_select], leds[led_select].blue + goingDirection[led_select]);
             }
+            ledToBe = leds[led_select].blue + goingDirection[led_select] * skipUp[led_select];
+            if (ledToBe > 255) {
+              ledToBe = 255;
+            } else if (ledToBe <= 0) {
+              ledToBe = 0;
+              pulsing[led_select] = false;
+            }
+            leds[led_select] = CRGB(ledToBe, ledToBe, ledToBe);
             break;
         }
-        delayCounter[led_select] = pulseDelay[led_select];
+        delayCounter[led_select] = pulseLength[led_select];
       }
     }
   }
